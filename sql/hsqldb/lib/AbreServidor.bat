88df419b34f2a08fdaeacf4a8351e9ea9752a0ea
dlqseronato <diegolquints@hotmail.com>
2018-08-26 22:00:48 -0300

    https://github.com/dlqseronato/GestaoRHDocker.git

--- sql/hsqldb/lib/AbreServidor.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\A0070047\Documents\Universidade\ProjetosArquiteturaReferencia-master\hsqldb-2.4.0\hsqldb\lib
-java -cp hsqldb.jar org.hsqldb.server.Server --database.0 file:BDSistCorp --dbname.0 POLYGON_DB
