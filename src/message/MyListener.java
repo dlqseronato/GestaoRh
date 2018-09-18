a68002bf706d1d95453a7d0240fcb331cf55238f
PatrickO98 <patrick.oliveira98@live.com>
2018-09-18 20:51:15 -0300

    Mensageria

---
+++ src/message/MyListener.java
@@ -0,0 +1,20 @@
+package message;
+
+import org.apache.catalina.tribes.ChannelMessage;
+import org.apache.catalina.tribes.MessageListener;
+
+public class MyListener implements MessageListener {
+
+	@Override
+	public boolean accept(ChannelMessage arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void messageReceived(ChannelMessage arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
